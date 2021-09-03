29a2aa8bb7ab10702dee769ee681a9baeeb52ec1
Aaron <aaron@sajatypeworks.com>
2021-09-03 11:21:36 -0700

    Adding the GASP table to the font

--- build.py
+++ build.py
@@ -1,11 +1,18 @@
 from fontTools.ttLib.ttFont import newTable
 from fontmake import __main__
 from fontTools.ttLib import TTFont, newTable
-import shutil, subprocess, glob, sys
+import shutil, subprocess, glob
 from pathlib import Path
 
 print ("[RocknRoll] Generating TTFs")
 __main__.main(("-g","sources/RocknRollOne.glyphs", "-o","ttf",))
+
+def GASP_set(font:TTFont):
+    if "gasp" not in font:
+        font["gasp"] = newTable("gasp")
+        font["gasp"].gaspRange = {}
+    if font["gasp"].gaspRange != {65535: 0x000A}:
+        font["gasp"].gaspRange = {65535: 0x000A}
 
 for font in Path("master_ttf").glob("*.ttf"):
     modifiedFont = TTFont(font)
@@ -21,28 +28,9 @@ for font in Path("master_ttf").glob("*.ttf"):
     modifiedFont["name"].addMultilingualName({'ja':'Regular'}, modifiedFont, nameID = 2, windows=True, mac=False)
     modifiedFont["head"].flags |= 1 << 3        #sets flag to always round PPEM to integer
 
+    GASP_set(modifiedFont)
     modifiedFont.save("fonts/ttf/"+str(font).split("/")[1])
 
 shutil.rmtree("instance_ufo")
 shutil.rmtree("master_ufo")
 shutil.rmtree("master_ttf")
-
-try:
-    if sys.argv[1] == "--autohinting":
-        for font in Path("fonts/ttf/").glob("*.ttf"):
-            print ("["+str(font).split("/")[2][:-4]+"] Autohinting")
-            fontName = str(font)
-            hintedName = fontName[:-4]+"-hinted.ttf"
-            subprocess.check_call(
-                [
-                    "ttfautohint",
-                    "--stem-width",
-                    "nsn",
-                    fontName,
-                    hintedName,
-                ]
-            )
-
-            shutil.move(hintedName, fontName)
-except IndexError:
-    pass
